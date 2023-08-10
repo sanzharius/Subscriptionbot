206877e813b2ab69293fa66689b5ce0799a0e3f9
sanzhar <sanzharius1999@gmail.com>
2023-08-10 11:04:46 +0600

    subs commit 2.4

--- main.go
+++ main.go
@@ -6,38 +6,41 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	log "github.com/sirupsen/logrus"
 	"net/http"
+	"subscriptionbot/bot"
+	"subscriptionbot/config"
+	"subscriptionbot/database"
+	"subscriptionbot/httpclient"
+	"subscriptionbot/logger"
 )
 
 func main() {
-	cfg, err := NewConfig(".env")
+	cfg, err := config.NewConfig(".env")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	InitLog(cfg)
+	logger.InitLog(cfg)
 
-	httpWeatherClient := NewHTTPClient()
-	weatherClient := NewWeatherClient(cfg, httpWeatherClient)
+	httpWeatherClient := httpclient.NewHTTPClient()
+	weatherClient := httpclient.NewWeatherClient(cfg, httpWeatherClient)
 
-	db := NewDbClient(cfg)
+	db := database.NewDbClient(cfg)
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	httpTgClient := NewHTTPClient()
+	httpTgClient := httpclient.NewHTTPClient()
 	tgClient, err := tgbotapi.NewBotAPIWithClient(cfg.TelegramBotTok, "https://api.telegram.org/bot%s/%s", httpTgClient)
 	if err != nil {
 		log.Fatal(err)
 	}
 	tgClient.Debug = true
 
-	tgBot, err := NewBot(cfg, weatherClient, tgClient, db)
+	tgBot, err := bot.NewBot(cfg, weatherClient, tgClient, db)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	tgBot.ReplyingOnMessages(ctx)
-	go tgBot.PushWeatherUpdates(ctx, cfg.Update)
-
-	//go tgBot.PushWeatherUpdates(ctx, db.Message)
+	go tgBot.PushWeatherUpdates(ctx)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", cfg.Port), nil))
 }
